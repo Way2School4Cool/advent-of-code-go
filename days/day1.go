ef077b1cf80b0599a0d0dc8789184429ccfa6784
Laika Sky <laika@localhost.localdomain>
2025-12-01 18:15:28 -0500

    day 1 wip

---
+++ days/day1.go
@@ -0,0 +1,101 @@
+package days
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Day1 struct{}
+
+var inputFile = "input/day1.txt"
+
+func (d *Day1) Part1() int {
+	zeros := 0
+	position := 50
+
+	fileData, _ := os.Open(inputFile)
+
+	line := bufio.NewScanner(fileData)
+	for line.Scan() {
+		move := line.Text()
+		moveAmount, _ := strconv.Atoi(move[1:])
+
+		if move[0] == 'R' {
+			position += moveAmount
+		} else if move[0] == 'L' {
+			position -= moveAmount
+		}
+
+		for {
+			if position >= 100 {
+				position -= 100
+			} else if position < 0 {
+				position += 100
+			} else {
+				break
+			}
+		}
+
+		if position == 0 {
+			zeros++
+		}
+	}
+
+	return zeros
+}
+
+func (d *Day1) Part2() int {
+	zeros := 0
+	position := 50
+
+	fileData, _ := os.Open(inputFile)
+	defer fileData.Close()
+
+	line := bufio.NewScanner(fileData)
+	for line.Scan() {
+		move := line.Text()
+		moveAmount, _ := strconv.Atoi(move[1:])
+
+		if move[0] == 'R' {
+			for i := 0; i < moveAmount; i++ {
+				position++
+				if position >= 100 {
+					zeros++
+					position -= 100
+				}
+			}
+
+			println(fmt.Sprintf("Move: %s        Position: %d        Zeros: %d", move, position, zeros))
+
+			continue
+
+		} else if move[0] == 'L' {
+			startingPosition := position
+
+			for i := 0; i < moveAmount; i++ {
+				position -= 1
+				if position < 0 {
+					zeros++
+					position += 100
+				}
+			}
+
+			if startingPosition == 0 {
+				zeros--
+			}
+
+			if position == 0 {
+				zeros++
+			}
+
+			println(fmt.Sprintf("Move: %s        Position: %d        Zeros: %d", move, position, zeros))
+
+			continue
+		}
+
+	}
+
+	return zeros
+}
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import days "advent-of-code/days"
+
+var day = &days.Day1{}
+
+func main() {
+	resultPart1 := day.Part1()
+	resultPart2 := day.Part2()
+
+	println("Day 1 - Part 1 Result:", resultPart1)
+	println("Day 1 - Part 2 Result:", resultPart2)
+}
+
+/*
+	// Example usage of Day1
+	day1 := &days.Day1{}
+
+	resultPart1 := day1.Part1(input)
+	resultPart2 := day1.Part2(input)
+
+	println("Day 1 - Part 1 Result:", resultPart1)
+	println("Day 1 - Part 2 Result:", resultPart2)
+
+*/
